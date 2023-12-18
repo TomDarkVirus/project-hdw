98d122345982611678dae97016106065e7db406a
martijnboerHanze <m.boer@st.hanze.nl>
2023-12-18 10:29:28 +0100

    ad automation

---
+++ Scripts/Windows Server/AD.ps1
@@ -0,0 +1,17 @@
+# Script to automate Active Directory installation and configuration
+
+# Set your domain name and DSRM password
+$DomainName = "hwu.nl"
+$DSRMPassword = "Wachtwoord123"
+
+# Install Active Directory Domain Services
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -Verbose
+
+# Promote the server to a domain controller
+Install-ADDSForest -DomainName $DomainName -DomainMode 4 -ForestMode 4 -InstallDns -Force -Verbose
+
+# Set DSRM password
+Set-LocalAdministratorPassword -Password (ConvertTo-SecureString -AsPlainText $DSRMPassword -Force) -Verbose
+
+# Reboot the server
+Restart-Computer -Force -Verbose
