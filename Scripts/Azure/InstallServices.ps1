9691d285d7d4f7633e01fca1ff942e4cad0756d4
TomDarkVirus <101814706+TomDarkVirus@users.noreply.github.com>
2024-01-18 15:54:21 +0100

    Updated InstallServices.ps1

--- Scripts/Azure/InstallServices.ps1
+++ Scripts/Azure/InstallServices.ps1
@@ -1,5 +1,5 @@
 # Define parameters
-$domainName = "test.local"
+$domainName = "test.local.nl"
 $domainAdministrator = "administrator1"
 $domainAdminPassword = Read-Host -Prompt "Enter password for $domainAdministrator" -AsSecureString
 
@@ -7,9 +7,4 @@ $domainAdminPassword = Read-Host -Prompt "Enter password for $domainAdministrato
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 
 # Create a new Active Directory forest
-Install-ADDSForest -DomainName $domainName -DomainMode Default -ForestMode Default -CreateDnsDelegation:$false -Force:$true -InstallDns
-
-# Promote the server to a domain controller
-Install-ADDSDomainController -DomainName $domainName -InstallDns -Credential (New-Object System.Management.Automation.PSCredential($domainAdministrator, $domainAdminPassword)) -Force:$true
-
-Write-Host "Domain creation and promotion to domain controller completed successfully."
+Install-ADDSForest -DomainName $domainName -DomainNetBIOSName TEST -InstallDNS
