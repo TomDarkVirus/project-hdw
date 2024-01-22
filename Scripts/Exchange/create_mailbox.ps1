358d03985dd5122953a95de6c675f2fa6790dddd
martijnboerHanze <145106411+martijnboerHanze@users.noreply.github.com>
2024-01-22 13:08:13 +0100

    Update create_mailbox.ps1
    add quotum

--- Scripts/Exchange/create_mailbox.ps1
+++ Scripts/Exchange/create_mailbox.ps1
@@ -1,18 +1,28 @@
-﻿# Import the Exchange Management Shell module
+# Import the Exchange Management Shell module
 Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn
 
 # Get a list of users from Active Directory
-$users = Get-ADUser -Filter * -SearchBase "OU=Studenten,DC=banaan,DC=nl" -Properties DisplayName
+$users = Get-ADUser -Filter * -SearchBase "OU=Studenten,DC=banaan,DC=nl" -Properties DisplayName, UserPrincipalName, SamAccountName
 
-# Loop through each user and create a mailbox
+# Loop through each user and check if they already have a mailbox
 foreach ($user in $users) {
     $userPrincipalName = $user.UserPrincipalName
     $displayName = $user.DisplayName
 
     # Check if DisplayName is not null or empty
     if (-not [string]::IsNullOrWhiteSpace($displayName)) {
-        # Create the mailbox
-        Enable-Mailbox -Identity $userPrincipalName -Alias $user.SamAccountName -DisplayName $displayName
+        # Check if the mailbox already exists
+        $existingMailbox = Get-Mailbox -Identity $userPrincipalName -ErrorAction SilentlyContinue
+
+        if ($existingMailbox -eq $null) {
+            # Create the mailbox
+            Enable-Mailbox -Identity $userPrincipalName -Alias $user.SamAccountName -DisplayName $displayName
+
+            # Set mailbox quotas
+            Set-Mailbox -Identity $userPrincipalName -IssueWarningQuota 400mb -ProhibitSendQuota 500mb -ProhibitSendReceiveQuota 600mb -UseDatabaseQuotaDefaults $false
+        } else {
+            Write-Host "Skipping user $userPrincipalName because a mailbox already exists."
+        }
     } else {
         Write-Host "Skipping user $userPrincipalName because DisplayName is null or empty."
     }
