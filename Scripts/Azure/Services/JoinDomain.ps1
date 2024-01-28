dad08ef98fd14d80177ff2055896d41a5f55a668
TomDarkVirus <101814706+TomDarkVirus@users.noreply.github.com>
2024-01-28 17:00:49 +0100

    Updated Azure scripts

---
+++ Scripts/Azure/Services/JoinDomain.ps1
@@ -0,0 +1,20 @@
+# Set DNS server
+$dnsServer = "10.10.0.14"
+netsh interface ipv4 set dns "Ethernet" static $dnsServer
+
+# Specify domain information
+$domainName = "test.local.nl"
+$domainAdmin = "administrator1"
+$domainAdminPassword = "Wachtwoord123"
+
+# Join the server to the domain
+Add-Computer -DomainName $domainName -Credential (Get-Credential -UserName $domainAdmin -Message "Enter domain administrator credentials")
+
+# Install AD DS (Active Directory Domain Services) role
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+
+# Promote the server to a domain controller
+Install-ADDSDomainController -InstallDns -DomainName $domainName -Credential (Get-Credential -UserName $domainAdmin -Message "Enter domain administrator credentials") -Force -NoRebootOnCompletion
+
+# Reboot the server
+Restart-Computer -Force
--- Scripts/Azure/SetDNSServer.ps1
+++
@@ -1,3 +0,0 @@
-# Set DNS server
-$dnsServer = "10.10.0.14"
-netsh interface ipv4 set dns "Ethernet" static $dnsServer
