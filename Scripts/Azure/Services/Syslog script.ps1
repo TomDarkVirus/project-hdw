50e7d2bfe11db0e707b9ca2cff839fff002deb5c
Ivokantt <145106594+Ivokantt@users.noreply.github.com>
2024-01-29 23:10:09 +0100

    Syslog script installatie

---
+++ Scripts/Azure/Services/Syslog script.ps1
@@ -0,0 +1,11 @@
+﻿# Define the URL of the MSI package
+$msiUrl = "https://dl.ezfive.com/swng/SyslogWatcherSetup-6.5.7-win64.msi"
+
+# Define the path where you want to save the downloaded MSI file
+$downloadPath = "C:\Users\administrator1\Downloads\SyslogWatcherSetup-6.5.7-win64.msi"
+
+# Download the MSI file
+Invoke-WebRequest -Uri $msiUrl -OutFile $downloadPath
+
+# Install the MSI package
+Start-Process msiexec.exe -ArgumentList "/i $downloadPath /qn" -Wait
